36688a7ef78b7fe7372fce63b2994281ed53ed0e
Leonardo Perrone <liox2012@gmail.com>
2019-03-31 17:15:18 +0200

    layout changes

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  public year = '';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.year = new Date().getFullYear().toString();
   }
 
 }
